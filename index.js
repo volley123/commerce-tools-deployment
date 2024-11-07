4f61fc583ea5066f21bbba82242bf1a2c978dcbe
Varshil Desai <desaivarshil5@gmail.com>
2024-11-07 17:00:09 +0530

    updated github workflow in dev branch

--- index.js
+++ index.js
@@ -2,6 +2,6 @@
 exports.handler = async (event) => {
     return {
         statusCode: 200,
-        body: JSON.stringify("Hello from Lambda functions!"),
+        body: JSON.stringify("Hello from Lambda functions with new dev branch!"),
     };
 };
